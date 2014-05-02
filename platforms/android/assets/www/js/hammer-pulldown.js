72c59995fde593cfc2801ae6430e90920a23ad85
Tim Paul <timpaul@google.com>
2014-05-02 14:10:38 +1000

    more styles...

--- platforms/android/assets/www/js/hammer-pulldown.js
+++ platforms/android/assets/www/js/hammer-pulldown.js
@@ -12,8 +12,8 @@ function PullDown(element, pullDownElement) {
 
     // establish some fixed variables (basic settings)
     var TIMEOUT_VAL = 220;
-    var BREAKPOINT = 75;
-    var LOCK_OFFSET = 165;
+    var BREAKPOINT = 72;
+    var LOCK_OFFSET = 180;
     var TRANSLATE_RATIO = .4;
     var THROTTLE = 50;
 
